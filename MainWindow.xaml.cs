e3a0e0dbabc9f3826e4b7d22772f25e201332109
skyatgit <1218980814@qq.com>
2023-09-13 15:01:33 +0800

    初始化

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BLivehimeNTR;
+
+/// <summary>
+///     Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
