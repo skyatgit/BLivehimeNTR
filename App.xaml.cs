2fe7dfe8248b00608572246e03b364f24a0c17cf
skyatgit <1218980814@qq.com>
2023-09-14 16:37:56 +0800

    添加互斥锁保证同时只有一个实例

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,8 +1,24 @@
-﻿namespace BLivehimeNTR;
+﻿using System.Threading;
+using System.Windows;
+
+namespace BLivehimeNTR;
 
 /// <summary>
 ///     Interaction logic for App.xaml
 /// </summary>
 public partial class App
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _ = new Mutex(true, "BLivehimeNTR", out var createdNew);
+
+        if (!createdNew)
+        {
+            MessageBox.Show("不要重复启动本程序。");
+            Current.Shutdown();
+            return;
+        }
+
+        base.OnStartup(e);
+    }
 }
