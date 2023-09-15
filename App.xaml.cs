64bd9c294a1cd00faf307667ff3c5e0e65894329
skyatgit <1218980814@qq.com>
2023-09-15 23:37:33 +0800

    添加x86和x64的wpcap.dll和Packet.dll

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading;
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
 using System.Windows;
 
 namespace BLivehimeNTR;
@@ -22,6 +25,25 @@ public partial class App
             return;
         }
 
+        var x86TempFolderPath = Path.Combine(Path.GetTempPath(), "BLivehimeNTR", "x86");
+        var x64TempFolderPath = Path.Combine(Path.GetTempPath(), "BLivehimeNTR", "x64");
+        Environment.SetEnvironmentVariable("Path", $"{x86TempFolderPath};{Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Process)}", EnvironmentVariableTarget.Process);
+        Environment.SetEnvironmentVariable("Path", $"{x64TempFolderPath};{Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Process)}", EnvironmentVariableTarget.Process);
+        Directory.CreateDirectory(x86TempFolderPath);
+        Directory.CreateDirectory(x64TempFolderPath);
+        var assembly = Assembly.GetExecutingAssembly();
+        using var x86WpcapDll = assembly.GetManifestResourceStream("BLivehimeNTR.Npcap.x86.wpcap.dll");
+        using var x86PacketDll = assembly.GetManifestResourceStream("BLivehimeNTR.Npcap.x86.Packet.dll");
+        using var x64WpcapDll = assembly.GetManifestResourceStream("BLivehimeNTR.Npcap.x64.wpcap.dll");
+        using var x64PacketDll = assembly.GetManifestResourceStream("BLivehimeNTR.Npcap.x64.Packet.dll");
+        using var x86WpcapFileStream = new FileStream(Path.Combine(x86TempFolderPath, "wpcap.dll"), FileMode.Create);
+        using var x86PacketFileStream = new FileStream(Path.Combine(x86TempFolderPath, "Packet.dll"), FileMode.Create);
+        using var x64WpcapFileStream = new FileStream(Path.Combine(x64TempFolderPath, "wpcap.dll"), FileMode.Create);
+        using var x64PacketFileStream = new FileStream(Path.Combine(x64TempFolderPath, "Packet.dll"), FileMode.Create);
+        x86WpcapDll?.CopyTo(x86WpcapFileStream);
+        x86PacketDll?.CopyTo(x86PacketFileStream);
+        x64WpcapDll?.CopyTo(x64WpcapFileStream);
+        x64PacketDll?.CopyTo(x64PacketFileStream);
         base.OnStartup(e);
     }
 }
